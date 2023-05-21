5ced5ab55c46a8d9f75f968a6b0af706dd942e60
Владимир Лисунов <Fox140890@mail.ru>
2023-05-21 19:14:02 +0300

    init ms communicate

---
+++ social-net-backend/communications-service/src/test/java/ru/team38/communicationsservice/CommunicationsServiceTest.java
@@ -0,0 +1,12 @@
+package ru.team38.communicationsservice;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class CommunicationsServiceTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
