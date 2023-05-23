6cbdc6a51d6c0cbf2725273d09d2d6b3a222aa48
Alexandr Sodlatov <salele4715@mail.ru>
2023-05-23 09:44:40 +0300

    контроллер /api/v1/account конфиг jaсkson формата ZoneDateTime

---
+++ social-net-backend/user-service/src/main/java/ru/team38/userservice/dto/AccountSearchDto.java
@@ -0,0 +1,25 @@
+package ru.team38.userservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountSearchDto {
+  private Long id;
+  private boolean isDeleted;
+  private String ids;
+  private String blockedByIds;
+  private String author;
+  private String firstName;
+  private String lastName;
+  private String city;
+  private String country;
+  private boolean isBlocked;
+  private Integer ageTo;
+  private Integer ageFrom;
+}
